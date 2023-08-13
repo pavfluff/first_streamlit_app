1ad24a660660f322ff0df8248b7504fd1dbbaeae
Alena Edora <116326303+pavfluff@users.noreply.github.com>
2023-08-13 17:19:42 +0800

    Update streamlit_app.py
    Added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
 st.title('My Parents New Healthy Diner')
-st.text('Just writing sample text, see if something changes.')
+st.header('Breakfast Menu')
+st.text('Omega 3 & Blueberry Pancakes')
+st.text('Monggo Fridays')
+st.text('Hard-Boiled Free-Range Egg')
