d98b2b6e0d2db7e29aec690b648c418ccf4b72b6
Alena Edora <116326303+pavfluff@users.noreply.github.com>
2023-08-13 17:17:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
 st.title('My Parents New Healthy Diner')
-
-st.header('Breakfast Menu')
-st.text('Omega 3 & Blueberr Oatmeal')
+st.text('Just writing sample text, see if something changes.')
