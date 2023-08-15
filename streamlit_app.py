48ba28b1830f7947b9d4abf0f882f31d1bcc4725
Alena Edora <116326303+pavfluff@users.noreply.github.com>
2023-08-15 14:49:16 +0800

    Update streamlit_app.py
    turn json to data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,4 +22,8 @@ streamlit.dataframe(fruits_to_show)
 # new section for fruity vice
 streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json())
+# turning json into a datafram
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# showing the dataframe in the app
+streamlit.dataframe(fruityvice_normalized)
