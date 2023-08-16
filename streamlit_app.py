741f3ebeec8ba41c9633c2b95fc1ab851fcd77dc
Alena Edora <116326303+pavfluff@users.noreply.github.com>
2023-08-16 08:21:04 +0800

    Update streamlit_app.py
    make fruit choices from fruity vice dynamic

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,9 @@ streamlit.dataframe(fruits_to_show)
 
 # new section for fruity vice
 streamlit.header('Fruityvice Fruit Advice!')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
 # turning json into a datafram
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # showing the dataframe in the app
