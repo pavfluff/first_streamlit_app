975d1abc72bb7350ad4dabf7dad2da5ff04361b7
Alena Edora <116326303+pavfluff@users.noreply.github.com>
2023-08-16 10:08:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,9 +27,6 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
-  
-# don't run any streamlit until troubleshooted
-streamlit.stop()
 
 # snowflake metadata
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
